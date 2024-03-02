e3a79a42ce7c3518e5bb561941766ba66d677cca
Shivam kumar <shivamkumarsing.sk.2020@gmail.com>
2024-03-02 21:16:17 +0530

    all changes allright and app run well before starting backend

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const userModel = require("./users"); //importing model of users 
+
+const passport = require('passport');
+const localStrategy = require('passport-local');
+
+// passport.use(new localStrategy(userModel.authenticate()));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Robonox' });
 });
 
-router.get('/login',function(req, res){
-  res.render('login');
-})
-router.get('/signup',function(req, res){
-  res.render('signUp');
-})
+// router.get('/login',function(req, res){
+//   res.render('login');
+// })
+// router.get('/signup',function(req, res){
+//   res.render('signUp');
+// })
 
 module.exports = router;
